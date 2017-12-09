6b6b99a4de3c55b807cca2ecc635d02864ac09cd
Alexander R Torrijos <artorrijos@gmail.com>
2017-12-09 20:44:42 +0800

    Displays the path of the running jar file

---
+++ src/com/emma/workoutmanager/Helpers/Helpers.java
@@ -0,0 +1,36 @@
+package com.emma.workoutmanager.Helpers;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import com.emma.workoutmanager.WorkoutManager;
+
+public class Helpers {
+	
+	/**
+	 * Returns the path to the currently running jar file
+	 * 
+	 * @param <WorkoutManager>
+	 * @return String
+	 */
+	public static String getJarPath(Class<WorkoutManager> className) {
+		try {
+			return new File(className.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getPath();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns the current working directory
+	 * This is the directory where the jar file was executed not the location of the running jar file
+	 * 
+	 * @return String
+	 */
+	public static String getCwd() {
+		// Paths requires java.nio
+		// return Paths.get(".").toAbsolutePath().normalize().toString();
+		return System.getProperty("user.dir");
+	}
+}
--- src/com/emma/workoutmanager/WorkoutManager.java
+++ src/com/emma/workoutmanager/WorkoutManager.java
@@ -10,17 +10,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.emma.workoutmanager.Helpers.Helpers;
+
 public class WorkoutManager {
 
 	private static final long serialVersionUID = -2492878511255087306L;
 
 	public static void main(String[] args) {		
-		getJarPath();
-		System.out.println(getCwd());
-		return;
 		
 		// Initialize Frame
-		/*JFrame frame = new JFrame("Workout Manager");
+		JFrame frame = new JFrame("Workout Manager");
 		frame.setSize(600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -28,7 +27,7 @@ public class WorkoutManager {
 		initPanel(frame);
 		
 		// Display Frame
-		frame.setVisible(true);*/
+		frame.setVisible(true);
 	}
 	
 	private static void initPanel(JFrame frame) {
@@ -39,50 +38,10 @@ public class WorkoutManager {
 		label.setBounds(10, 20, 80, 25);
 		panel.add(label);
 		
-		
-		JLabel dirLabel = new JLabel();
+		JLabel dirLabel = new JLabel("Dir: " + Helpers.getJarPath(WorkoutManager.class));
+		dirLabel.setBounds(10, 40, 600, 25);
 		panel.add(dirLabel);
 		
 		frame.add(panel);
-	}
-	
-	/**
-	 * Returns the path to the currently running jar file
-	 * @return
-	 */
-	private static String getJarPath() {
-		// Dir of the running jar file
-				String jarPath = WorkoutManager.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-				try {
-					System.out.println(jarPath);
-					String decodedPath = URLDecoder.decode(jarPath, "UTF-8");
-					System.out.println(decodedPath);
-					
-				} catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				try {
-					File jarPathx = new File(WorkoutManager.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-					System.out.println("------------------");
-					System.out.println(jarPathx.getPath());
-				} catch (URISyntaxException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-		return null;
-	}
-	
-	/**
-	 * Returns the current working directory
-	 * This is the directory where the jar file was executed not the location of the running jar file
-	 * @return
-	 */
-	private static String getCwd() {
-		// Paths requires java.nio
-		// return Paths.get(".").toAbsolutePath().normalize().toString();
-		return System.getProperty("user.dir");
 	}
 }
